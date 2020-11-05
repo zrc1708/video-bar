4a38e6198bcc96101d13ef615d5e93d6c49ada8e
zrc1708 <1023054043@qq.com>
2020-11-05 23:48:25 +0800

    项目编写

--- loginrouter.js
+++ loginrouter.js
@@ -58,23 +58,19 @@ const loginrouter = new Router()
 // });
 
 // 获取mp3接口
-loginrouter.get('/getimage/:imageName', async (ctx, next) => {
-    const imageName = ctx.params.imageName.trim()
+loginrouter.get('/getvoice/:voiceName', async (ctx, next) => {
+    const voiceName = ctx.params.voiceName.trim()
     // 设置文件路径
-    let filePath = path.join(__dirname)+'/'+imageName
-	try {
-	    //读取文件
-        file = fs.readFileSync(filePath)
-	} catch (error) {
-		//如果服务器不存在请求的图片，返回默认图片
-	    filePath = path.join(__dirname, '/404/404.png')
-	    file = fs.readFileSync(filePath)    
-    }
+    let filePath = path.join(__dirname)+'/mp3/'+voiceName
+	
+    //读取文件
+    file = fs.readFileSync(filePath)
+	
     //读取图片文件类型
     let mimeType = mime.lookup(filePath)
     //设置返回类型
     ctx.set('content-type', mimeType)
-    //返回图片
+    //返回
 	ctx.body = file
 });
 
@@ -111,10 +107,11 @@ loginrouter.post('/register', async ctx => {
     const {sex} = ctx.request.body
     const {age} = ctx.request.body
     const {email} = ctx.request.body
-    
+    const time = new Date().Format("yyyy-MM-dd hh:mm:ss");
+
     const connection = await Mysql.createConnection(mysql_nico)
-    let sql = `INSERT INTO user ( username, password , isadm , sex , age , email ) 
-                VALUES ( '${username}', '${password}', 0, ${sex} , ${age} ,'${email}');`;
+    let sql = `INSERT INTO user ( username, password , isadm , sex , age , email ,birthtime) 
+                VALUES ( '${username}', '${password}', 0, ${sex} , ${age} ,'${email}','${time}');`;
     const [rs] = await connection.query(sql);
 
     // 为新用户初始化语音设置
@@ -152,7 +149,7 @@ loginrouter.post('/publish', async ctx => {
     const [rs] = await connection.query(sql);
 
     // 语音合成，保存到本地文件
-    client.text2audio(content, {
+    await client.text2audio(content, {
         per: rs[0].per,
         spd: rs[0].spd,
         pit: rs[0].pit,
@@ -186,16 +183,21 @@ loginrouter.post('/publish', async ctx => {
     }
 });
 
-// 获取用户所有文章接口
-loginrouter.get('/getmyarticle/:id', async ctx => {
-    let id = ctx.params.id
+// 获取所有文章接口
+loginrouter.get('/getallarticles', async ctx => {
     
     const connection = await Mysql.createConnection(mysql_nico)
-    const sql = `Select * from article where userid = ${id};`
+    const sql = `Select article.id,article.title,article.content,article.commits,
+                article.views, article.voice, article.posttime, article.userid,
+                user.username ,user.sex ,user.age ,user.age ,user.email ,user.birthtime,
+                avatar.filename
+                from article ,user ,avatar 
+                where article.userid = user.id and article.userid = avatar.userid
+                order by article.id desc;`
     const [rs] = await connection.query(sql);
     connection.end(function (err) {})
 
-    if (rs.length > 0) {
+    if (rs) {
         ctx.body = {
             code: 200,
             tips: '获取设置成功',
@@ -205,6 +207,56 @@ loginrouter.get('/getmyarticle/:id', async ctx => {
         ctx.body = {
             code: 201,
             tips: '获取设置失败'
+        }
+    }
+});
+
+// 获取用户所有文章接口
+loginrouter.get('/getmyarticle/:id', async ctx => {
+    let id = ctx.params.id
+    
+    const connection = await Mysql.createConnection(mysql_nico)
+    const sql = `Select * from article where userid = ${id} order by id desc;`
+    const [rs] = await connection.query(sql);
+    connection.end(function (err) {})
+
+    if (rs) {
+        ctx.body = {
+            code: 200,
+            tips: '获取设置成功',
+            rs
+        }
+    } else {
+        ctx.body = {
+            code: 201,
+            tips: '获取设置失败'
+        }
+    }
+});
+
+// 通过id获取文章接口
+loginrouter.get('/getarticle/:id', async ctx => {
+    let id = ctx.params.id
+    const connection = await Mysql.createConnection(mysql_nico)
+    const sql = `Select article.id,article.title,article.content,article.commits,
+                article.views, article.voice, article.posttime, article.userid,
+                user.username ,user.sex ,user.age ,user.age ,user.email ,user.birthtime,
+                avatar.filename
+                from article ,user ,avatar 
+                where article.userid = user.id and article.userid = avatar.userid and article.id = ${id};`
+    const [rs] = await connection.query(sql);
+    connection.end(function (err) {})
+
+    if (rs) {
+        ctx.body = {
+            code: 200,
+            tips: '获取文章成功',
+            rs
+        }
+    } else {
+        ctx.body = {
+            code: 201,
+            tips: '获取文章失败'
         }
     }
 });
@@ -261,43 +313,187 @@ loginrouter.post('/changevoicesetting', async ctx => {
 
 // 头像上传接口
 loginrouter.post('/uploadimage', async (ctx, next) => {
-    console.log(ctx.request.files.file);
     let file = ctx.request.files.file
-    // let file,fileNames
-    // // 获取保存文件用的文件名, 获取文件
-    // if(ctx.request.files.file.length){
-    //     file = [...ctx.request.files.file]
-    // }else{
-    //     file = [ctx.request.files.file]
-    // }
-    // fileNames = [...ctx.request.body.fileNames.split(',')]
-
     const filename = Date.now()+''+Math.floor(Math.random()*10)+'.webp'
+
     // 保存文件
-    // 创建可读流
     const reader = fs.createReadStream(file.path);
     let filePath = path.join(__dirname+'/images') + `/${filename}`;
-    // 创建可写流
     const upStream = fs.createWriteStream(filePath);
-    // 可读流通过管道写入可写流
     reader.pipe(upStream);
 
-    // var imageUrl = []
-    // // 将文件信息写入数据库
-    // const connection = await Mysql.createConnection(mysql_nico)
-    // fileNames.forEach(async (item)=>{
-    //     let filepath = path.join(__dirname+'/images')+ `/${item}`
-    //     // 顺便生成图片文件的访问地址
-    //     let url = baseurl+'/getimage/'+item
-    //     imageUrl.push(url)
-    //     const sql = `INSERT INTO image (filename, path,url) VALUES ( '${item}', '${filepath}','${url}');`
-    //     const [rs] = await connection.query(sql);
-    // })
-    // connection.end(function(err){})
+    const {id} = ctx.request.body
+    const {haveAvatar} = ctx.request.body
 
-    return ctx.body = {
-        message:"上传成功！",
-        code:200,
-    };
+    // // 将文件信息写入数据库
+    const connection = await Mysql.createConnection(mysql_nico)
+    let sql
+    if(haveAvatar === 'true'){
+        // 如果用户有之前有头像，更新数据库
+        sql = `UPDATE avatar SET filename = '${filename}' WHERE userid = ${id};`
+        // 删除旧头像
+        const {oldFileName} = ctx.request.body
+        fs.unlink(path.join(__dirname+'/images') + `/${oldFileName}`, (err) => {
+            if(err) throw err;
+        });
+    }else{
+        // 如果用户没有头像，插入数据库
+        sql = `INSERT INTO avatar (filename, userid) VALUES ( '${filename}', ${id});`
+    }
+    const [rs] = await connection.query(sql);
+    connection.end(function(err){})
+
+    if (rs.affectedRows == 1) {
+        ctx.body = {
+            code: 200,
+            tips: '上传成功',
+            filename
+        }
+    } else {
+        ctx.body = {
+            code: 201,
+            tips: '上传失败'
+        }
+    }
 });
+
+// 根据id获取用户头像
+loginrouter.get('/getimagename/:id', async (ctx, next) => {
+    const id = ctx.params.id.trim()
+        
+    const connection = await Mysql.createConnection(mysql_nico)
+    const sql = `Select * from avatar where userid = ${id};`
+    const [rs] = await connection.query(sql);
+    connection.end(function (err) {})
+
+    if (rs.length>0) {
+        ctx.body = {
+            code: 200,
+            tips: '获取头像成功',
+            rs:rs[0]
+        }
+    } else {
+        ctx.body = {
+            code: 210,
+            tips: '此用户没有上传过头像'
+        }
+    }
+});
+
+// 获取图片接口
+loginrouter.get('/getimage/:imageName', async (ctx, next) => {
+    const imageName = ctx.params.imageName.trim()
+    // 设置文件路径
+    let filePath = path.join(__dirname)+'/images/'+imageName
+
+	try {
+	    //读取文件
+        file = fs.readFileSync(filePath)
+	} catch (error) {
+		//如果服务器不存在请求的图片，返回默认图片
+	    filePath = path.join(__dirname, '/404/404.png')
+	    file = fs.readFileSync(filePath)    
+    }
+    //读取图片文件类型
+    let mimeType = mime.lookup(filePath)
+    //设置返回类型
+    ctx.set('content-type', mimeType)
+    //返回图片
+	ctx.body = file
+});
+
+// 文章增加阅读量接口
+loginrouter.get('/addviews/:id/:views', async (ctx, next) => {
+    const id = ctx.params.id
+    const views = ctx.params.views
+    
+    const connection = await Mysql.createConnection(mysql_nico)
+    const sql = `UPDATE article SET views = '${Number(views)+1}' WHERE id = ${id};`
+    const [rs] = await connection.query(sql);
+    connection.end(function(err){})
+
+    ctx.body = {
+        code: 200,
+        tips: 'ok'
+    }
+});
+
+// 评论发表接口
+loginrouter.post('/publishcommit', async ctx => {
+    const {content} = ctx.request.body
+    const {userid} = ctx.request.body    
+    const {articleid} = ctx.request.body    
+    const {commits} = ctx.request.body    
+    const time = new Date().Format("yyyy-MM-dd hh:mm:ss");
+    const filename = Date.now()+''+Math.floor(Math.random()*10)+'.mp3'
+
+    const connection = await Mysql.createConnection(mysql_nico)
+
+    // 获取用户的语音设置
+    let sql = `SELECT * FROM voicesetting WHERE userid = '${userid}';`
+    const [rs] = await connection.query(sql);
+
+    // 语音合成，保存到本地文件
+    await client.text2audio(content, {
+        per: rs[0].per,
+        spd: rs[0].spd,
+        pit: rs[0].pit,
+        vol: rs[0].vol,
+    }).then(function (result) {
+        if (result.data) {
+            fs.writeFileSync(`mp3/${filename}`, result.data);
+        } else {
+            console.log('语音合成失败: ' + JSON.stringify(result));
+        }
+    }, function (err) {
+        console.log(err);
+    });
+
+    // 插入数据库
+    sql = `INSERT INTO commit ( content , voice , posttime ,userid ,articleid) 
+                VALUES ('${content}','${filename}','${time}',${userid},${articleid});`;
+    const [rss] = await connection.query(sql);
+
+    // 跟新文章与评论相关的内容
+    sql = `UPDATE article SET commits = '${Number(commits)+1}' ,
+            latestcommit='${new Date().Format("yyyy-MM-dd hh:mm:ss")}' WHERE id = ${articleid};`
+    const [rsss] = await connection.query(sql);
+
+    connection.end(function (err) {})
+
+    if (rss.affectedRows == 1) {
+        ctx.body = {
+            code: 200,
+            tips: '评论成功'
+        }
+    } else {
+        ctx.body = {
+            code: 201,
+            tips: '评论失败'
+        }
+    }
+});
+
+// 获取文章评论接口
+loginrouter.get('/getcommits/:id', async (ctx, next) => {
+    const id = ctx.params.id
+    
+    const connection = await Mysql.createConnection(mysql_nico)
+    const sql = `Select commit.id,commit.content,
+                commit.voice, commit.posttime, commit.userid,commit.articleid,
+                user.username ,user.sex ,user.age ,user.age ,user.email ,user.birthtime,
+                avatar.filename
+                from commit ,user ,avatar 
+                where commit.userid = user.id and commit.userid = avatar.userid and commit.articleid = ${id}
+                order by commit.id desc;`
+    const [rs] = await connection.query(sql);
+    connection.end(function(err){})
+
+    ctx.body = {
+        code: 200,
+        tips: 'ok',
+        rs
+    }
+});
+
 module.exports = loginrouter
